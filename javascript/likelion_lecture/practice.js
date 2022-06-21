65037cca0cdb29eac61d3e50b792148dced1d033
Liamyoum <83207838+Liamtsy@users.noreply.github.com>
2022-06-21 13:09:02 +0900

    Add files via upload, update

--- javascript/likelion_lecture/practice.js
+++ javascript/likelion_lecture/practice.js
@@ -1,79 +1,39 @@
-document.getElementById('lion').style.color = 'red';
-document.getElementById('tiger').style.color = 'blue';
+// Several DOM methods
+document.getElementsByTagName('li')[0].style.color = 'red';
+document.getElementsByClassName('land')[1].style.color = 'blue';
 document.getElementById('bear').style.color = 'green';
-document.getElementById('eagle').style.color = 'purple';
-
-document.getElementsByTagName('li')[0].style.backgroundColor =
-	'black';
-document.getElementsByClassName('land')[1].style.backgroundColor =
-	'yellow';
-document.querySelectorAll('.land')[2].style.backgroundColor = 'pink';
-
-// innerHTML
-document.querySelectorAll('.fly')[0].innerHTML = 'hawk'; // fly 클래스 가진 index 0번 놈을 hawk 문자열로 교체
+document.querySelectorAll('.fly')[0].style.color = 'purple';
+// animals 변수의 innerHTML에 우변 추가
 const animals = document.getElementById('animals');
+animals.innerHTML += `<li class = 'water'>dolpine</li>`;
+document.querySelector('.water').style.color = 'orange';
+// box class 가진 첫 번째 놈에게 purple이라는 클래스 추가
+document.querySelectorAll('.box')[0].classList.add('purple');
+// 삭제
+document.querySelectorAll('.box')[0].classList.remove('purple');
+// classList.toggle은 그 클래스가 있으면 삭제, 없으면 추가 해줌
+document.querySelectorAll('.box')[0].classList.toggle('yellow');
+document.querySelectorAll('.box')[0].classList.toggle('yellow');
 
-animals.innerHTML += "<li class = 'fly'>eagle</li>"; // animals 아이디 가진 엘리먼트 안에 문자열 추가해주기
-
-// classList
-document.querySelectorAll('.box')[0].classList.add('purple'); // box 클래스 가진 index 0번 놈에게 purple이란 클래스 추가
-document.querySelectorAll('.box')[0].classList.remove('purple'); // box 클래스 가진 index 0번 놈에게 purple이란 클래스 삭제
-document.querySelectorAll('.box')[0].classList.toggle('yellow'); // box 클래스 가진 index 0번 놈에게 yellow라는 클래스 없으면 추가, 있으면 삭제 해줌
-document.querySelectorAll('.box')[0].classList.toggle('yellow'); // box 클래스 가진 index 0번 놈에게 yellow라는 클래스 없으면 추가, 있으면 삭제 해줌
-
-// Event 사건 발생 시키는거 (동작에 반응)
+// Several Event type
 // EventTarget.addEventListener('eventType', function, useCapture);
 document.getElementById('btn').addEventListener('click', function () {
-	console.log('wow you made it!!');
-	alert('good!');
+	console.log('click!!');
 });
 let num = 0;
 document
 	.getElementById('plus')
 	.addEventListener('click', function () {
 		num++;
-		document.getElementById('number').innerHTML = num;
+		document.getElementById('num').innerHTML = num;
 	});
 document
 	.getElementById('minus')
 	.addEventListener('click', function () {
 		num--;
-		document.getElementById('number').innerHTML = num;
+		document.getElementById('num').innerHTML = num;
 	});
 document.querySelector('.bar').addEventListener('click', function () {
-	document.querySelector('.bar').innerHTML = 'You clicked!';
-	document.querySelector('.new').classList.toggle('show');
+	document.querySelector('.newBar').classList.toggle('show');
+	document.querySelector('.bar').innerHTML = 'Click again!';
 });
-
-// Event Object 이벤트 객체
-document
-	.querySelectorAll('.list')[0]
-	.addEventListener('click', function (e) {
-		tabBtn(e.target.dataset.id);
-	});
-
-function tabBtn(a) {
-	document
-		.querySelectorAll('.tab-button')
-		[a].addEventListener('click', function () {
-			for (
-				let i = 0;
-				i < document.querySelectorAll('.tab-button').length;
-				i++
-			) {
-				document
-					.querySelectorAll('.tab-button')
-					[i].classList.remove('here');
-				document
-					.querySelectorAll('.tab-content')
-					[i].classList.remove('show');
-			}
-
-			document
-				.querySelectorAll('.tab-button')
-				[a].classList.add('here');
-			document
-				.querySelectorAll('.tab-content')
-				[a].classList.add('show');
-		});
-}
